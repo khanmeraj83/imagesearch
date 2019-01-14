841f25ba61789a9ab70dac41673d99b3ba37c8fa
Meraj khan <khanmeraj83@gmail.com>
2019-01-14 18:24:45 +0500

    Updates

---
+++ precache-manifest.55d51e79e3c79ef72ae5deb54e14db26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8349674581a0f35b4c78",
+    "url": "/imagesearch/static/js/runtime~main.83496745.js"
+  },
+  {
+    "revision": "428d03d26a66e7d30076",
+    "url": "/imagesearch/static/js/main.428d03d2.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/imagesearch/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "428d03d26a66e7d30076",
+    "url": "/imagesearch/static/css/main.eb56a801.chunk.css"
+  },
+  {
+    "revision": "4a7787d93c2e4af855bb686c4c720791",
+    "url": "/imagesearch/index.html"
+  }
+];
